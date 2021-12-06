5c313f435019bd5362e572051b5627bff8968511
Vivian Gomez <viviangomez0@gmail.com>
2021-12-06 17:09:34 -0500

    commit tree 1d9f7b58c13672ada00aa940acc610f4c55e8913

--- main.js
+++ main.js
@@ -3,8 +3,25 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const clickedHearts=document.querySelectorAll(".like-glyph");
 
+function likeCallback(e){
+  const heart = e.target;
+  mimicServerCall()
+   .then(function(serverMessage){
+     alert("You notified the server!");
+     alert(serverMessage);
+    heart.innerText= EMPTY_HEART[heart.innerText];
+    heart.style.color= FULL_HEART[heart.style.color];
 
+   })
+   .catch(function(error) {
+     alert("something went wrong")
+   });
+}
+
+for (const glyph of clickedHearts ){
+  glyph.addEventListener("click", likeCallback);}
 
 
 //------------------------------------------------------------------------------
